cf582c215b5db224ce5712c2913240ff7dcfd23a
rohsyl <syzin12@gmail.com>
2021-05-05 17:27:05 +0200

    add guard param to middleware rule

--- src/Middlewares/AclMiddleware.php
+++ src/Middlewares/AclMiddleware.php
@@ -10,30 +10,28 @@ class AclMiddleware
 {
     public function handle($request, Closure $next, $inputs)
     {
-        if (Auth::guest()) {
-            throw UnauthorizedException::notLoggedIn();
-        }
 
         $parameters = explode('$', $inputs);
         $role = $parameters[0] ?? '';
         $guard = $parameters[1] ?? null;
 
+        $auth = auth();
+        if(isset($guard)) {
+            $auth = $auth->guard($guard);
+        }
+
+        if ($auth->guest()) {
+            throw UnauthorizedException::notLoggedIn();
+        }
 
         $roles = is_array($role)
             ? $role
             : explode('|', $role);
 
-
         $roles = array_map(function($item){
             $item = explode(':', $item);
             return [$item[0] => $item[1] ?? ACL_ALLOW];
         }, $roles);
-
-
-        $auth = auth();
-        if(isset($guard)) {
-            $auth = $auth->guard($guard);
-        }
 
         if (! $auth->user()->hasAclAny($roles)) {
             throw UnauthorizedException::forPermissions();
